bf93d3a152e4caacda8f4eefa3ad50968bc57e0a
HeLLo2 <popt0@naver.com>
2023-01-03 11:20:20 +0900

    first commit

---
+++ src/main/java/shop/itbook/itbookbatch/ItbookBatchApplication.java
@@ -0,0 +1,13 @@
+package shop.itbook.itbookbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItbookBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItbookBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/shop/itbook/itbookbatch/ItbookBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.itbook.itbookbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItbookBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
