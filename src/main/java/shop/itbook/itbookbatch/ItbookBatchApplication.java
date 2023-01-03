8f57f1aed4db60cd23ecb06ffccc45f88c18218e
HeLLo2 <popt0@naver.com>
2023-01-03 16:37:07 +0900

    [feat] 젠킨스 자동배포 테스트 젠킨스 연결 테스트 진행발

--- src/main/java/shop/itbook/itbookbatch/ItbookBatchApplication.java
+++ src/main/java/shop/itbook/itbookbatch/ItbookBatchApplication.java
@@ -9,7 +9,7 @@ public class ItbookBatchApplication {
     public static void main(String[] args) {
         System.out.println("Jenkins 배포");
         System.out.println("Jenkins 배포2");
-        System.out.println("Jenkins 배포3");
+        System.out.println("제발");
         SpringApplication.run(ItbookBatchApplication.class, args);
     }
 
