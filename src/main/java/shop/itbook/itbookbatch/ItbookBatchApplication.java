39b9d3f3c1f02846606b82be073710cb787b6295
yihoney <109622@naver.com>
2023-01-03 11:34:54 +0900

    [feat] 남의 거 변경 포크 테스트

--- src/main/java/shop/itbook/itbookbatch/ItbookBatchApplication.java
+++ src/main/java/shop/itbook/itbookbatch/ItbookBatchApplication.java
@@ -9,7 +9,7 @@ public class ItbookBatchApplication {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("please");
-        System.out.println("내가 한 번 해본다");
+        System.out.println("내가 한 번 해본다 포크로도");
         SpringApplication.run(ItbookBatchApplication.class, args);
     }
 
