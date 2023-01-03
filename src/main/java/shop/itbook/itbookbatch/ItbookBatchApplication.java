8e0a12004130c806605a86ac9f25581b9ae1f6f4
yihoney <109622@naver.com>
2023-01-03 11:32:32 +0900

    [feat] 남의 거 테스트

--- src/main/java/shop/itbook/itbookbatch/ItbookBatchApplication.java
+++ src/main/java/shop/itbook/itbookbatch/ItbookBatchApplication.java
@@ -9,6 +9,7 @@ public class ItbookBatchApplication {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("please");
+        System.out.println("내가 한 번 해본다");
         SpringApplication.run(ItbookBatchApplication.class, args);
     }
 
